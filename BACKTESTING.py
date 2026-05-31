f2cb4222661a374635d872d9dabcae344e8ff14c
mochstanpda-hub <mochstanpda@gmail.com>
2026-05-31 12:58:39 +0200

    Update BACKTESTING.py

--- BACKTESTING.py
+++ BACKTESTING.py
@@ -60,7 +60,7 @@ except:
 # ==============================================================================
 # VERZE A AUTO-UPDATE
 # ==============================================================================
-VERSION = "1.5.61"
+VERSION = "1.5.62"
 
 # CHANGELOG — co je nového v každé verzi (parsováno při aktualizaci)
 # Formát: verze | Změna 1; Změna 2; Změna 3
